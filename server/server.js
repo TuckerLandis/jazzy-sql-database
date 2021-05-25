5cd228216624f1f0bbf0a308340daddfd19572cd
TuckerLandis <landistuckerc@gmail.com>
2021-05-25 16:58:16 -0500

    first get

--- server/server.js
+++ server/server.js
@@ -11,17 +11,47 @@ app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
 
+// pg stuff
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'jazzy-sql',
+    host: 'localhost',
+    port: 5432,
+})
 
+pool.on('connect', () => {
+    console.log('Connected to PG');
+})
+
+pool.on('error', (err) => {
+    console.log('Error to PG', err);
+})
+
+//artist routes
 
 app.get('/artist', (req, res) => {
     console.log(`In /songs GET`);
    // res.send(artistList);
+    let queryText = `SELECT * FROM "artist" ORDER BY "birthdate" DESC;`
+
+    pool.query(queryText)
+    .then( (result) => {
+        res.send(result.rows);
+    }).catch (err => {
+        console.log(err);
+        res.sendStatus(500);
+    })
+
+
 });
 
 app.post('/artist', (req, res) => {
     artistList.push(req.body);
   //  res.sendStatus(201);
 });
+
+// song routes
 
 app.get('/song', (req, res) => {
     console.log(`In /songs GET`);
