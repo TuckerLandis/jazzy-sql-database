e874e400921e88439583a4caa267090e159514c6
TuckerLandis <landistuckerc@gmail.com>
2021-05-26 09:47:37 -0500

    get/post functional

--- database.sql
+++ database.sql
@@ -4,34 +4,37 @@ CREATE TABLE "artists" (
     "year_born" date
 );
 
+// table creation 
 
 CREATE TABLE artist (
-id SERIAL,
+id SERIAL PRIMARY KEY,
 name varchar(80),
 birthdate date
 );
 
 CREATE TABLE song (
-id SERIAL,
+id SERIAL PRIMARY KEY,
 title varchar(255),
 length varchar(10),
 released date
 );
 
-INSERT INTO "artist"("id", "name", "birthdate") VALUES(1, 'Ella Fitzgerald', '04-25-1917');
 
-INSERT INTO "artist"("id", "name", "birthdate") VALUES(2, 'Dave Brubeck', '12-06-1920');
+// initial inserts
 
-INSERT INTO "artist"("id", "name", "birthdate") VALUES(3, 'Miles Davis', '05-26-1926');
+INSERT INTO "artist"("name", "birthdate") VALUES('Ella Fitzgerald', '04-25-1917');
 
-INSERT INTO "artist"("id", "name", "birthdate") VALUES(4, 'Esperanza Spaulding', '10-18-1984');
+INSERT INTO "artist"("name", "birthdate") VALUES('Dave Brubeck', '12-06-1920');
 
-SELECT * FROM "artist";
+INSERT INTO "artist"("name", "birthdate") VALUES('Miles Davis', '05-26-1926');
 
-INSERT INTO "song"("id", "title", "length", "released") VALUES(1, 'Take Five', '5:24', '1959-09-29');
+INSERT INTO "artist"("name", "birthdate") VALUES('Esperanza Spaulding', '10-18-1984');
 
-INSERT INTO "song"("id", "title", "length", "released") VALUES(2, 'So What', '9:22', '1959-08-17');
 
-INSERT INTO "song"("id", "title", "length", "released") VALUES(3, 'Black Gold', '5:17', '2012-02-01');
 
-SELECT * FROM "song";
+INSERT INTO "song"("title", "length", "released") VALUES('Take Five', '5:24', '1959-09-29');
+
+INSERT INTO "song"("title", "length", "released") VALUES('So What', '9:22', '1959-08-17');
+
+INSERT INTO "song"("title", "length", "released") VALUES('Black Gold', '5:17', '2012-02-01');
+
--- server/server.js
+++ server/server.js
@@ -4,14 +4,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static('server/public'));
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
 
-// pg stuff
+// pg config
 const pg = require('pg');
 const Pool = pg.Pool;
 const pool = new Pool({
@@ -29,22 +31,19 @@ pool.on('error', (err) => {
 })
 
 //artist routes
-
 app.get('/artist', (req, res) => {
     console.log(`In /songs GET`);
-   // res.send(artistList);
+    
     let queryText = `SELECT * FROM "artist" ORDER BY "birthdate" DESC;`
 
     pool.query(queryText)
-    .then( (result) => {
-    
-        res.send(result.rows);
-    }).catch (err => {
-        console.log(err);
-        res.sendStatus(500);
-    })
+        .then((result) => {
 
-
+            res.send(result.rows);
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        })
 });
 
 app.post('/artist', (req, res) => {
@@ -53,42 +52,48 @@ app.post('/artist', (req, res) => {
     let queryText = `INSERT INTO "artist" ("name", "birthdate") 
     VALUES ($1, $2);`
 
-    // this adds fine, but the ID of a new artist to the DB was '1', which already exists, thought serial prevented this but 
-
     let values = [req.body.name, req.body.birthdate];
 
     pool.query(queryText, values)
-    .then( (result) => {
-        res.sendStatus(201);
-    }).catch(err => {
-        console.log(err);
-        res.sendStatus(500);
-        
-    })
-
-
-
+        .then((result) => {
+            res.sendStatus(201);
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        })
 });
 
-// song routes
 
+// song routes
 app.get('/song', (req, res) => {
     console.log(`In /songs GET`);
     let queryText = `SELECT * FROM "song" ORDER BY "title" DESC;`
 
     pool.query(queryText)
-    .then( (result) => {
-    
-        res.send(result.rows);
-    }).catch (err => {
-        console.log(err);
-        res.sendStatus(500);
-    })
+        .then((result) => {
+
+            res.send(result.rows);
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        })
 });
+
 
 app.post('/song', (req, res) => {
-   // songList.push(req.body);
-   // res.sendStatus(201);
+    console.log('req.body', req.body)
+
+    let queryText = `INSERT INTO "song" ("title", "length", "released") 
+    VALUES ($1, $2, $3);`
+
+    let values = [req.body.title, req.body.released, req.body.released];
+
+    pool.query(queryText, values)
+        .then((result) => {
+            res.sendStatus(201);
+
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        })
 });
-
-
